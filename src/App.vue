154c15578e270d484cb7f90d23686aab997fcdd1
carlmenke <carlmenke13@gmail.com>
2022-10-12 10:13:22 -0700

    created my first component in vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoodComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoodComponent from './components/GoodComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    GoodComponent
+}
 }
 </script>
 
---
+++ src/components/GoodComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>A Good Component</h3>
+    </div>
+</template>
+
+<script> //must always export an object
+    export default {
+        name: 'GoodComponent'
+    }
+</script>
+
+<styles>
+    h3 {
+        color: #80cbc4;
+    }
+</styles>
